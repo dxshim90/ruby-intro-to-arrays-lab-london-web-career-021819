97e7559ef760614a77ac8e35844164f1f7eb1d50
Daniel Shimield <dxshim90@gmail.com>
2019-01-29 12:48:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,5 +1,7 @@
 def instantiate_new_array
   instantiate_new_array = Array.new
-  instantiate_new_array = Array.new(2)
 end
 
+def array_with_two_elements
+  instantiate_new_array = Array.new(2)
+end
